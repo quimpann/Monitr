c19765183ec184e2433024028a21111dbfd4bd93
Nichole Quimpan <quimpannb@students.nu-laguna.edu.ph>
2025-03-17 09:23:53 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <base href="http://localhost/webprog/Monitr_variant/">
+    <meta http-equiv="refresh" content="1; url=Monitr/pages/landing.html">
+   
+    
+    <title> Monitr </title>
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="Monitr/assets/css/global.css">
+    <link rel="stylesheet" href="Monitr/assets/css/redirect.css">
+</head>
+<body>
+    <p>Redirecting to Monitr...</p>
+</body>
+</html>
